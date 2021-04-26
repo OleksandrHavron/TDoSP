060be266813771253120bcfde41562f18191c687
Alexander Korniichuk <world.kor.films@gmail.com>
2021-04-26 22:02:22 +0300

    Add SVG_handler

--- library/admin.py
+++ library/admin.py
@@ -1,18 +1,103 @@
 from django.contrib import admin
+from django import forms
 
 from .models import Category, SubCategory, App
+
+import re
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        exclude = []
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    form = CategoryForm
     prepopulated_fields = {"slug": ("name",)}
+
+    def save_model(self, request, obj, form, change):
+        handle_uploaded_file(request.FILES['svg_icon'])
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
+    def save_model(self, request, obj, form, change):
+        handle_uploaded_file(request.FILES['svg_icon'])
+        super().save_model(request, obj, form, change)
+
 
 @admin.register(App)
 class AppAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+
+
+# def handle_uploaded_file(f):
+#     svg_code = ''
+#     for line in f:
+#         svg_code += line.decode()
+#
+#     ch_count = len(svg_code)
+#
+#     pattern1 = re.compile('<svg.*>', re.DOTALL)
+#     pattern2 = re.compile('<path.*>', re.DOTALL)
+#
+#     svg_result = pattern1.findall(svg_code, re.DOTALL)[0]
+#
+#     pattern3 = re.compile('class=[\'\"].*[\'\"]')
+#
+#     print(pattern3.findall(svg_result))
+#
+#     if pattern3.search(svg_result, re.DOTALL):
+#         svg_handled = pattern3.sub('class="app-list__svg"', svg_result, re.DOTALL)
+#     else:
+#         svg_handled = re.sub('<svg', '<svg class="app-list__svg"', svg_result, re.DOTALL)
+#
+#     svg_code = re.sub(str(svg_result), str(svg_handled), svg_code, re.DOTALL)
+#
+#     path_result = pattern2.findall(svg_code, re.DOTALL)
+#
+#     path_handled = []
+#
+#     for i in path_result:
+#         print(pattern3.findall(i))
+#         print(i)
+#         if pattern3.findall(i):
+#             path_handled.append(pattern3.sub('class="app-list__svg-path"', i, re.DOTALL))
+#         else:
+#             path_handled.append(re.sub('<path', '<path class="app-list__svg-path"', i, re.DOTALL))
+#
+#     for i in range(len(path_handled)):
+#         svg_code = re.sub(str(path_result[i]), str(path_handled[i]), svg_code, re.DOTALL)
+#
+#     f.seek(0)
+#
+#     f.write((' ' * ch_count).encode())
+#     f.seek(0)
+#
+#     f.write(svg_code.encode())
+
+def handle_uploaded_file(f):
+    svg_code = ''
+    for line in f:
+        svg_code += line.decode()
+
+    ch_count = len(svg_code)
+
+    pattern = re.compile(' class=[\'\"]\S*[\'\"]')
+
+    svg_code = pattern.sub('', svg_code)
+
+    svg_code = re.sub('<svg', '<svg class="app-list__svg"', svg_code, re.DOTALL)
+    svg_code = re.sub('<path', '<path class="app-list__svg-path"', svg_code, re.DOTALL)
+
+    f.seek(0)
+
+    f.write((' ' * ch_count).encode())
+    f.seek(0)
+
+    f.write(svg_code.encode())
--- library/models.py
+++ library/models.py
@@ -1,12 +1,30 @@
 from django.db import models
 from django.urls import reverse
 
+import xml.etree.cElementTree as et
+
+
+def is_svg(f):
+    """
+    Check if provided file is svg
+    """
+    f.seek(0)
+    tag = None
+    try:
+        for event, el in et.iterparse(f, ('start',)):
+            tag = el.tag
+            break
+    except et.ParseError:
+        pass
+    return tag == '{http://www.w3.org/2000/svg}svg'
+
 
 class Category(models.Model):
     name = models.CharField(max_length=55)
     description = models.TextField()
-    image = models.ImageField(upload_to='categories', blank=True)
+    image = models.ImageField(upload_to='categories/images', blank=True)
     slug = models.SlugField(verbose_name='slug', max_length=50, unique=True, blank=False)
+    svg_icon = models.FileField(upload_to='categories/svg_icons', blank=True, validators=[is_svg])
 
     class Meta:
         verbose_name = 'Категорія'
@@ -23,8 +41,9 @@ class SubCategory(models.Model):
     name = models.CharField(max_length=55)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subcategories')
     description = models.TextField()
-    image = models.ImageField(upload_to='apps/', blank=True)
+    image = models.ImageField(upload_to='subcategories/images/', blank=True)
     slug = models.SlugField(verbose_name='slug', max_length=50, unique=True, blank=False)
+    svg_icon = models.FileField(upload_to='subcategories/svg_icons', blank=True)
 
     class Meta:
         verbose_name = 'Підкатегорія'
